d9d3fcba2b817ea733b646e85e62c0700d444684
anastasiia <anastasiia@lifeslice.io>
2018-01-03 16:37:41 +0200

    change the name of app instance

--- app/src/main/java/com/spacebanana/funwithgeofence/FunWithGeofenceApplication.java
+++ app/src/main/java/com/spacebanana/funwithgeofence/FunWithGeofenceApplication.java
@@ -7,17 +7,17 @@ import com.spacebanana.funwithgeofence.di.AppModule;
 import com.spacebanana.funwithgeofence.di.DaggerAppComponent;
 
 public class FunWithGeofenceApplication extends Application {
-    private static FunWithGeofenceApplication mInstance;
+    private static FunWithGeofenceApplication appInstance;
     private AppComponent component;
 
     public static synchronized FunWithGeofenceApplication get() {
-        return mInstance;
+        return appInstance;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mInstance = this;
+        appInstance = this;
 
         if (component == null) {
             component = DaggerAppComponent.builder().appModule(new AppModule(FunWithGeofenceApplication.get())).build();
